6fdbd6b6f43beb0d821258103517a2d259a052cd
Denis <47882549+sncodeGit@users.noreply.github.com>
2020-07-25 22:21:21 +0300

    Create technical_work.php

---
+++ technical_work.php
@@ -0,0 +1,23 @@
+<html>
+  <head>
+    <title>Сайт находится на обслуживании</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+  </head>
+  <body style="font-family: Arial;">
+    <div style="margin:0 auto; text-align:center">
+      <h1 style="text-align:center; font-size:46px; margin-top:100px;">
+        Сайт на обслуживании, зайдите позже!
+      </h1>
+      <p style="font-size:20px; margin-top:50px; border-bottom: 1px solid #555; padding-bottom:40px;">
+        В настоящее время на сайте ведутся технические работы.
+         Зайдите пожалуйста через несколько часов, сайт станет ещё лучше!
+      </p>
+      <div style="color: #555; font-size:24px;">
+        <p>
+          Команда разработчиков и инженеров
+          <?php echo $_SERVER["SERVER_NAME"]; ?>
+        </p>
+      </div>
+    </div>
+  </body>
+</html>
